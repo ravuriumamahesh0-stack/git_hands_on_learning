cfdc4e99762ccd46614220993db3c4a0c5b42821
Ravuri Uma Maheswar Rao <ravuriumamahesh0@gmail.com>
2026-03-16 00:08:02 +0530

    first all files

---
+++ arthimatic/add.py
@@ -0,0 +1,12 @@
+
+def add(a,b=10):  
+    a = a 
+    b = b
+    return a+b
+
+# when we import file the entire file runs once so this calling also execute once to that i wrote this 
+if __name__ == "__main__":
+    print(__name__)
+    print(type(__name__))
+    print(add(3,4))
+
---
+++ arthimatic/sub.py
@@ -0,0 +1,9 @@
+
+def sub(a,b=10):  
+    a = a 
+    b = b
+    return a-b
+
+if __name__ == "__main__": # when we import file the entire file runs once so this calling also execute once to that i wrote this 
+
+    print(sub(10,4))
